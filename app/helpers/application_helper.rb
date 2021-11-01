d6e38217b8f24f832eb25e254694c9a73f2db6e9
Ravi Singh <ravi@appsimpact.com>
2021-11-01 13:14:35 +0530

    Working on events UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def formatted_datetime(datetime)
    datetime.strftime('%d %b %Y %H:%M:%S %p')
   end
+
+  def flash_class(level)
+    case level
+      when :success then "alert alert-success"
+      when :notice then "alert alert-success"
+      when :alert then "alert alert-danger"
+      when :error then "alert alert-danger"
+      when :warning then "alert alert-warning"
+    end
+  end
 end
